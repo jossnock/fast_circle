582119d8e8583b1b42ac26fcd5a32228f87927ba
JossNockolds <19121867@hlnsc.ac.uk>
2024-03-23 08:40:44 +0000

    added side-scrolling

--- game.py
+++ game.py
@@ -2,9 +2,8 @@ import pygame
 
 # pygame setup:
 pygame.init()
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 730
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+SCREEN_WIDTH, SCREEN_HEIGHT = SCREEN.get_size()
 CLOCK = pygame.time.Clock()
 running = True
 FPS = 60
@@ -12,16 +11,24 @@ background_colour = (232,190,232)
 SCREEN.fill(background_colour)
 
 #circle properties:
-circle_x = SCREEN_HEIGHT // 2
-circle_y = SCREEN_HEIGHT // 2
+circle_centre_x = SCREEN_HEIGHT // 2
+circle_centre_y = SCREEN_HEIGHT // 2
 circle_radius = 64
-circle_width = 8
+circle_width = 0 # 0 for a filled circle
 circle_x_velocity = 0
 circle_y_velocity = 0
-circle_x_past = 99
-circle_y_past = 99
-CIRCLE_X_VELOCITY_MULTIPLIER = 1
-CIRCLE_Y_VELOCITY_MULTIPLIER = 1
+circle_x_acceleration = 2
+circle_y_acceleration = 2
+circle_x_past = 100
+circle_y_past = 100
+GRAVITY = 0.25
+HORIZONTAL_GRAVITY = 1
+
+circle_x_velocity_max = 20
+circle_x_velocity_min = 0
+
+circle_y_velocity_max = 10
+circle_y_velocity_min = -10
 
 # circle colours:
 COLOUR_INTERVAL = 48
@@ -46,54 +53,56 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    keys = pygame.key.get_pressed()
+    keys_pressed = pygame.key.get_pressed()
+    circle_x_past = circle_centre_x
+    circle_y_past = circle_centre_y
 
-    circle_x_velocity_previous = circle_x_velocity
-    circle_y_velocity_previous = circle_y_velocity
-
-    # accelerates the circle in the direction it is moving in:
-    if (keys[pygame.K_SPACE]):
-        if circle_x_velocity_previous >= 0 and circle_y_velocity_previous >= 0:
-            circle_x_velocity += CIRCLE_X_VELOCITY_MULTIPLIER
-            circle_y_velocity += CIRCLE_Y_VELOCITY_MULTIPLIER
-        if circle_x_velocity_previous >= 0 and circle_y_velocity_previous < 0:
-            circle_x_velocity += CIRCLE_X_VELOCITY_MULTIPLIER
-            circle_y_velocity -= CIRCLE_Y_VELOCITY_MULTIPLIER
-        if circle_x_velocity_previous < 0 and circle_y_velocity_previous >= 0:
-            circle_x_velocity -= CIRCLE_X_VELOCITY_MULTIPLIER
-            circle_y_velocity += CIRCLE_Y_VELOCITY_MULTIPLIER
-        if circle_x_velocity_previous < 0 and circle_y_velocity_previous < 0:
-            circle_x_velocity -= CIRCLE_X_VELOCITY_MULTIPLIER
-            circle_y_velocity -= CIRCLE_Y_VELOCITY_MULTIPLIER
-
-    # deccelerates the circle in the direction it is moving in:
-    if (keys[pygame.K_BACKSPACE]):
-        if circle_x_velocity != 0:
-            if circle_x_velocity > 0:
-                circle_x_velocity -= CIRCLE_X_VELOCITY_MULTIPLIER
-            elif circle_x_velocity < 0:
-                circle_x_velocity += CIRCLE_X_VELOCITY_MULTIPLIER
-        if circle_y_velocity != 0:
-            if circle_y_velocity > 0:
-                circle_y_velocity -= CIRCLE_Y_VELOCITY_MULTIPLIER
-            elif circle_y_velocity < 0:
-                circle_y_velocity += CIRCLE_Y_VELOCITY_MULTIPLIER
-
-    
-
-    # fill the space of the circle before movement with background_colour to wipe away anything from last frame:
-    pygame.draw.circle(SCREEN, background_colour, (circle_x_past,circle_y_past), circle_radius)
+    # fill the space of the circle before movement with background_colour to wipe away anything from last frame: 
+    # WORK IN PROGRESS
+    pygame.draw.circle(SCREEN, background_colour, (circle_x_past,circle_y_past), circle_radius, circle_width)
 
     # render the circle:
-    pygame.draw.circle(SCREEN, circle_colour_primary, (circle_x,circle_y), circle_radius, circle_width, True, False, True)
-    pygame.draw.circle(SCREEN, circle_colour_secondary, (circle_x,circle_y), circle_radius, circle_width, False, True, False, True)
+    pygame.draw.circle(SCREEN, circle_colour_primary, (circle_centre_x,circle_centre_y), circle_radius, circle_width, True, False, True)
+    pygame.draw.circle(SCREEN, circle_colour_secondary, (circle_centre_x,circle_centre_y), circle_radius, circle_width, False, True, False, True)
     
-    circle_x_past = circle_x
-    circle_y_past = circle_y
+    # move the circle according to the player's input:
+    if keys_pressed[pygame.K_w]:
+       circle_y_velocity += -1 * circle_y_acceleration
+    if keys_pressed[pygame.K_s]:
+       circle_y_velocity += 1 * circle_y_acceleration
+    if keys_pressed[pygame.K_a]:
+       circle_x_velocity += -1 * circle_x_acceleration
+    if keys_pressed[pygame.K_d]:
+       circle_x_velocity += 1 * circle_x_acceleration
+    
+    # keep the velocities within a range:
+    if circle_x_velocity > circle_x_velocity_max:
+        circle_x_velocity = circle_x_velocity_max
+    if circle_x_velocity < circle_x_velocity_min:
+        circle_x_velocity = circle_x_velocity_min
+    if circle_y_velocity > circle_y_velocity_max:
+        circle_y_velocity = circle_y_velocity_max
+    if circle_y_velocity < circle_y_velocity_min:
+        circle_y_velocity = circle_y_velocity_min
 
-    # move the circle according to its velocity:
-    circle_x += circle_x_velocity
-    circle_y += circle_y_velocity
+    circle_y_velocity += GRAVITY
+    circle_x_velocity += HORIZONTAL_GRAVITY
+
+    circle_centre_x += circle_x_velocity
+    circle_centre_y += circle_y_velocity
+
+    # keep the circle within the borders of the screen:w
+    if circle_centre_y - circle_radius < 0:
+        circle_y_velocity = 0
+        circle_centre_y = 0 + circle_radius # stops the circle from getting stuck
+    if circle_centre_y + circle_radius > SCREEN_HEIGHT:
+        circle_y_velocity = 0
+        circle_centre_y = SCREEN_HEIGHT - circle_radius # stops the circle from getting stuck
+
+    # transitions to the next screen
+    if circle_centre_x - circle_radius > SCREEN_WIDTH:
+        circle_centre_x = -circle_radius
+        SCREEN.fill(background_colour)
 
     pygame.display.flip() # flip() the display to put your work on screen
 
